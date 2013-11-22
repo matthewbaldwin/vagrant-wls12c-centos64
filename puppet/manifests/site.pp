25b285434dbc9581db4218bd5877495ed84d7c59
Matthew Baldwin <matthewbaldwin@comcast.net>
2013-11-22 10:29:07 -0500

    create a swapfile and it to fstab and disable iptables

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -63,6 +63,37 @@ class os2 {
   sysctl { 'net.core.wmem_default':         ensure => 'present', permanent => 'yes', value => '262144',}
   sysctl { 'net.core.wmem_max':             ensure => 'present', permanent => 'yes', value => '1048576',}
 
+  # create a swapfile and it to fstab
+  
+  exec { "create swap file":
+    command => "/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192",
+    creates => "/var/swap.1",
+  }
+
+  exec { "attach swap file":
+    command => "/sbin/mkswap /var/swap.1 && /sbin/swapon /var/swap.1",
+    require => Exec["create swap file"],
+    unless => "/sbin/swapon -s | grep /var/swap.1",
+  }
+
+  #add swap file entry to fstab
+  exec {"add swapfile entry to fstab":
+    command => "/bin/echo >>/etc/fstab /var/swap.1 swap swap defaults 0 0",
+    require => Exec["attach swap file"],
+    user => root,
+    unless => "/bin/grep '^/var/swap.1' /etc/fstab 2>/dev/null",
+  }
+  
+  
+  # turn off iptables
+  
+  service { iptables:
+        enable    => false,
+        ensure    => false,
+        hasstatus => true,
+  }
+
+
 }
 
 class wls12{
